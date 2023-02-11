88b94b76c0055001a48ae91bec5f03854c12e7a4
duyanhreal <kutepingping10@gmail.com>
2023-02-12 02:48:13 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '😇 À nhô BunBun 😇',
+    introDesc: `Không được bấm vào!!! Cảnh báo dất áp lựccc`,
+    btnIntro: 'OK',
+    title: 'Valentine dồi, thời gian đẹp nhất để công khai đã đến dồiiii ',
+    desc: 'Giờ em có đồng ý give mi your forever khônggg',
+    btnYes: 'Suy nghí',
+    btnNo: 'Không',
+    question:'Đúng người sai thời điểm, em từng nói thía. Nếu đã là đúng người vậy thì tuyệt rồi, anh sẽ dành thời gian của mình cho em tới khi em sẵn sàng move on, ý anh là lúc em thật sự move on ấy nhé, nghiêm túc!!!\nYea cam on vi da den\n!!!!!!',
+    reasonPlaceholder: 'Điền câu cảm thán zô đây nhoé',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Em đã sẵn soàng gòi!!',
+    mess: 'Yea, chúc Bunbun zui zẻ zới tình eo này, đây không phải là passion, đây là commitment, bonding ọp lớp',
+    //messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ko',
+    messLink: 'https://m.me/duyanh.real'
+}
